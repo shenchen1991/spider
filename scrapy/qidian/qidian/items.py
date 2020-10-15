02038b2b2c71cc1d43ce6271d5ac78010a1cfc99
shenchen <572631358@qq.com>
2020-10-15 19:29:08 +0800

    111

--- scrapy/qidian/qidian/items.py
+++ scrapy/qidian/qidian/items.py
@@ -8,6 +8,21 @@
 import scrapy
 
 
+class BookItem(scrapy.Item):
+    book_id = scrapy.Field
+    book_url = scrapy.Field()
+    book_cover = scrapy.Field()
+    book_name = scrapy.Field()
+    author = scrapy.Field()
+    tags = scrapy.Field()
+    summary = scrapy.Field()
+
+
+class JuanItem(scrapy.Item):
+    juan_id = scrapy.Field
+    title = scrapy.Field
+
+
 class QidianItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
